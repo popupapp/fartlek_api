4edee676ef03629c2d9fc9a523a280c34dfbfa77
Jason Humphries <humphriesj@gmail.com>
2014-03-23 16:50:54 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FartlekApi::Application.config.secret_key_base = '2e4be7252b67df3b8676d8ac52bb043d743663fe64972c12388191e3432fa653f601d69720ab4cac8850897c0458fd99bc3b37350e945415efce1f5a78fd8b7d'
